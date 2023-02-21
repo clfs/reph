06c8e179ac0081100230eee8f05c34d6dfad4850
Calvin Figuereo-Supraner <mail@calvin.page>
2023-02-21 15:52:12 -0800

    Add chess.TestNewGameFromFEN

--- chess/notation_test.go
+++ chess/notation_test.go
@@ -3,9 +3,24 @@ package chess
 import "testing"
 
 func TestGame_FEN(t *testing.T) {
-	g := NewGame()
-	got := g.FEN()
-	if got != StartingFEN {
-		t.Errorf("got %q, want %q", got, StartingFEN)
+	got := NewGame().FEN()
+	want := StartingFEN
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestNewGameFromFEN(t *testing.T) {
+	game, err := NewGameFromFEN(StartingFEN)
+	if err != nil {
+		t.Fatalf("NewGameFromFEN: %v", err)
+	}
+
+	got := game.FEN()
+	want := StartingFEN
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
