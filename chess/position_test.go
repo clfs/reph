025d7d7315275712da965fe229928359be5b49ff
Calvin Figuereo-Supraner <mail@calvin.page>
2023-02-25 09:16:27 -0800

    Remove chess.TestCastleRights

--- chess/position_test.go
+++ chess/position_test.go
@@ -1,36 +1,8 @@
 package chess
 
 import (
-	"math/rand"
-	"reflect"
 	"testing"
-	"testing/quick"
 )
-
-// Generate implements testing/quick.Generator for CastleRights.
-func (CastleRights) Generate(r *rand.Rand, size int) reflect.Value {
-	return reflect.ValueOf(CastleRights(r.Intn(16)))
-}
-
-func TestCastleRights(t *testing.T) {
-	setThenGet := func(a, b CastleRights) bool {
-		a.Set(b)
-		return a.Get(b) || b == 0
-	}
-
-	if err := quick.Check(setThenGet, nil); err != nil {
-		t.Error(err)
-	}
-
-	clearThenGet := func(a, b CastleRights) bool {
-		a.Clear(b)
-		return !a.Get(b)
-	}
-
-	if err := quick.Check(clearThenGet, nil); err != nil {
-		t.Error(err)
-	}
-}
 
 func TestPosition_Move(t *testing.T) {
 	p := NewPosition()
